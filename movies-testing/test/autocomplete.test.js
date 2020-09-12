bc5a8db74e6bed13c365fd3eb236d0e7081c9099
Isaac Sokari <isaacsokari@gmail.com>
2020-09-13 00:21:56 +0100

    dispatched a fake DOM input using dispatchEvent

--- movies-testing/test/autocomplete.test.js
+++ movies-testing/test/autocomplete.test.js
@@ -1,4 +1,6 @@
-it('it shows an autocomplete', () => {
+beforeEach(() => {
+  document.querySelector('#target').innerHTML = '';
+
   createAutoComplete({
     root: document.querySelector('#target'),
     fetchData() {
@@ -12,8 +14,20 @@ it('it shows an autocomplete', () => {
       return movie.Title;
     },
   });
+});
 
+it('dropdown is not active on page load', () => {
   const dropdown = document.querySelector('.dropdown');
 
   expect(dropdown.className).to.not.include('is-active');
+});
+
+it('after input, dropdown shows up', () => {
+  document.querySelector('input').value = 'avengers';
+
+  document.querySelector('input').dispatchEvent(new Event('input'));
+
+  const dropdown = document.querySelector('.dropdown');
+  console.log(dropdown.className);
+  expect(dropdown.className).to.include('is-active');
 });
