4d1ced46e4138c3b7e5906c342f07312c24830e0
Isaac Sokari <isaacsokari@gmail.com>
2020-09-12 19:55:25 +0100

    added test for map and implemented map

--- index.js
+++ index.js
@@ -4,4 +4,12 @@ module.exports = {
       func(arr[i], i);
     }
   },
+  map(arr, func) {
+    const newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      let value = arr[i];
+      newArr.push(func(value));
+    }
+    return newArr;
+  },
 };
--- index.test.js
+++ index.test.js
@@ -1,13 +1,27 @@
-const _ = require('./index');
+const { forEach, map } = require('./index');
 
 let sum = 0;
 
-_.forEach([1, 2, 3], (value) => {
+forEach([1, 2, 3], (value) => {
   sum += value;
 });
 
-console.log(sum);
-
 if (sum !== 6) {
   throw new Error('Expected sum of array to equal 6');
+}
+
+const result = map([1, 2, 3], (value) => {
+  return value * 2;
+});
+
+if (result[0] !== 2) {
+  throw new Error(`Expected 2 but found  ${result[0]}`);
+}
+
+if (result[1] !== 4) {
+  throw new Error(`Expected 4 but found  ${result[1]}`);
+}
+
+if (result[2] !== 6) {
+  throw new Error(`Expected 6 but found  ${result[2]}`);
 }
