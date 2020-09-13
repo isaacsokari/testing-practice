ea93760937202b6e84a4612b6d7bd6620b51f11f
Isaac Sokari <isaacsokari@gmail.com>
2020-09-14 00:19:29 +0100

    implemented step two of the breadth first search - collecting test files

--- tme/index.js
+++ tme/index.js
@@ -5,8 +5,8 @@ const Runner = require('./runner');
 const runner = new Runner();
 
 async function run() {
-  const results = await runner.collectFiles(process.cwd());
-  console.log(results);
+  await runner.collectFiles(process.cwd());
+  console.log(runner.testFiles);
 }
 
 run();
