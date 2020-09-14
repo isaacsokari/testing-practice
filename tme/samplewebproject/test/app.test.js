ff0f8769c5a1f026830f2667882af70749f94f4d
Isaac Sokari <isaacsokari@gmail.com>
2020-09-14 13:09:32 +0100

    added a test to check header innerText

--- tme/samplewebproject/test/app.test.js
+++ tme/samplewebproject/test/app.test.js
@@ -9,5 +9,24 @@ it('has a text input', async () => {
 
   const input = dom.window.document.querySelector('input');
 
-  assert.strictEqual(input.type, 'submit', 'Document should have a text input');
+  assert.strictEqual(input.type, 'text', 'Document should have a text input');
+});
+
+it('shows a success message with a valid email', async () => {
+  const dom = await render('index.html');
+
+  const input = dom.window.document.querySelector('input');
+  input.value = 'sadfasfdasfhas@asdf.asdf';
+
+  dom.window.document
+    .querySelector('form')
+    .dispatchEvent(new dom.window.Event('submit'));
+
+  const header = dom.window.document.querySelector('h1');
+  
+  assert.strictEqual(
+    header.innerText,
+    'Valid email',
+    'Header Value on correct submission should be "Valid email"!'
+  );
 });
